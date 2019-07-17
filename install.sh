ab90614077ed282c846cda7c358ac9f64bfe8946
Chase Conklin <developer@chasemconklin.com>
2019-07-16 17:07:49 -0700

    Set up install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo "Welcome to Chase's Computer!"
 
+CONFIG_DIR=${HOME}/.dotfiles
+
 cd $HOME
 
 # Install Homebrew
@@ -9,10 +11,10 @@ cd $HOME
 
 # Install homebrew bundle to execute the Brewfile
 brew tap homebrew/bundle
-brew bundle --file=${HOME}/.dotfiles/Brewfile
+brew bundle --file=${CONFIG_DIR}/Brewfile
 
 # Link vimrc and install vim plugins
-ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
+ln -s ${CONFIG_DIR}/.vimrc ${HOME}/.vimrc
 vim +PlugInstall
 
 # Install Oh-My-Zsh
@@ -20,5 +22,15 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Install Spacehip Theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Set up dotfiles
+ln -s ${CONFIG_DIR}/.zshrc.osx ${HOME}/.zshrc
+ln -s ${CONFIG_DIR}/.gitconfig ${HOME}/.gitconfig
+ln -s ${CONFIG_DIR}/.gitignore_global ${HOME}/.gitignore_global
+ln -s ${CONFIG_DIR}/.tmux.conf ${HOME}/.tmux.conf
+
+# Install tmuxinator
+gem install tmuxinator
+
+echo "Setup complete for Chase's Computer!"
