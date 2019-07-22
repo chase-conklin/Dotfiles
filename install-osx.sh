0c25527cf50f2e1aa4d530a3b44a12511af7bf24
Chase Conklin <developer@chasemconklin.com>
2019-07-22 14:31:39 -0700

    Added more if statements to the install script so that the script can be run multiple times if needed

--- install-osx.sh
+++ install-osx.sh
@@ -9,6 +9,8 @@ cd $HOME
 # Install Homebrew
 if ! type "brew" > /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew is already installed"
 fi
 
 # Install homebrew bundle to execute the Brewfile
@@ -24,14 +26,29 @@ ln -s ${CONFIG_DIR}/.vimrc ${HOME}/.vimrc
 vim -c ":qa"
 
 # Set zsh to default
-chsh -s $(which zsh);
+if [ "$SHELL" != /bin/zsh ]
+then
+  chsh -s $(which zsh);
+else
+  echo "Zsh is already the default shell"
+fi
 
 # Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo "Oh-My-Zsh is already installed"
+fi
+
 ln -s ${CONFIG_DIR}/.zshrc.osx ${HOME}/.zshrc
 
 # Install Spacehip Theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "${ZSH_CUSTOM}/themes/spaceship-prompt"
+if [ ! -d "${HOME}/.oh-my-zsh/custom/themes/spaceship-prompt" ]; then
+  git clone https://github.com/denysdovhan/spaceship-prompt.git "${ZSH_CUSTOM}/themes/spaceship-prompt"
+else
+  echo "Spaceship theme is already installed"
+fi
+
 ln -s "${ZSH_CUSTOM}/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM}/themes/spaceship.zsh-theme"
 
 # Set up dotfiles
@@ -40,6 +57,10 @@ ln -s ${CONFIG_DIR}/.gitignore_global ${HOME}/.gitignore_global
 ln -s ${CONFIG_DIR}/.tmux.conf ${HOME}/.tmux.conf
 
 # Install tmuxinator
-gem install tmuxinator
+if ! type "tmuxinator" > /dev/null; then
+  gem install tmuxinator
+else
+  echo "Tmuxinator is already installed"
+fi
 
 echo "Setup complete for Chase's Computer!"
