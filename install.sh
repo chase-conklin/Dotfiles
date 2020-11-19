ae755d81e83e8906a39f791ecfbb97b70330289d
Chase Conklin <developer@chasemconklin.com>
2020-11-18 23:47:40 -0800

    Update install file and include tmuxinator.sh file

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ cd $HOME
 
 # Install Homebrew
 if ! type "brew" > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
   echo "Homebrew is already installed"
 fi
@@ -27,8 +27,9 @@ else
 fi
 
 # Link vimrc and install vim plugins
+rm ${HOME}/.vimrc
 ln -s ${CONFIG_DIR}/.vimrc ${HOME}/.vimrc
-vim -c ":qa"
+vim -c ":PlutInstall | :qa"
 
 # Set zsh to default
 if [ "$SHELL" != /bin/zsh ]
@@ -65,17 +66,5 @@ ln -s ${CONFIG_DIR}/.tmux.conf ${HOME}/.tmux.conf
 
 # Set up Karabiner Elements configuration
 ln -s ${CONFIG_DIR}/karabiner ${HOME}/.config
-
-# Install tmuxinator
-if ! type "tmuxinator" > /dev/null; then
-  gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  curl -sSL https://get.rvm.io | bash -s stable
-  gem install tmuxinator
-  mkdir ${HOME}/.bin
-  cd ${HOME}/.bin
-  curl -O https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh
-else
-  echo "Tmuxinator is already installed"
-fi
 
 echo "Setup complete for Chase's Computer!"
